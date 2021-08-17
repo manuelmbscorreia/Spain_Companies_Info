e850a511300fb35e347d69520c148e56453f4074
Manuel <manuelmbscorreia@gmail.com>
2021-08-17 05:03:38 +0100

    Hello

--- infocif.py
+++ infocif.py
@@ -15,32 +15,15 @@ import urllib3
 import re
 import numpy as np
 
-
 #Preparar lista de cidades a selecionar e para Input
-print("Pretende onter nomes das empresas de que região de Espanha?)
-print("Escolha uma ou mais opções, escritas em minusculas sem acentos, separadas por virgula e um espaço.")
-
-lista_ubicacion = ["espanha", "andalucia", "aragon", "canarias", "castilla la mancha", "castilla y leon",
-                   "cataluna", "ceuta", "comunidad de madrid", "comunidad foral de navarra", "comunitat valenciana",
-                   "extremadura", "galicia", "islas baleares", "la rioja", "melilla", "pais vasco",
-                   "principado de asturias", "region de murcia"]
-
-
-escolha = input("toda espanha, andalucia, aragon, canarias, castilla la mancha, castilla y leon, cataluna, ceuta, "
-                "comunidad de madrid, comunidad foral de navarra, comunitat valenciana, extremadura, galicia, "
-                "islas baleares, la rioja, melilla, pais vasco, principado de asturias, region de murcia:  "
-
-
-lista_escolha = escolha.split(",")
-
-
 
 
 #Setup Website
 
+#profile = webdriver.FirefoxProfile('/usr/local/bin/')
 browser = webdriver.Firefox()
-browser.get("https://codigopostal.ciberforma.pt/")
-time.sleep(7)
+browser.get("http://www.infocif.es/buscador/#/")
+time.sleep(10)
 
 
 #Get Element - Filter by region
@@ -48,77 +31,103 @@ path = "/html/body/div[1]/div/div[1]/div/div/div/div/section/div/div/div/div/div
 element = browser.find_element_by_xpath(path)
 
 #Click on Element
-actions = ActionChains(browser)
+actions = webdriver.ActionChains(browser)
 actions.move_to_element(element)
 actions.click(element)
 actions.perform()
 
-#Choose Region
-for a in lista_cidades:
+time.sleep(4)
 
-      #Get Element
-      index_div = int(lista_escolha.index(a)) + 1
-      path = f"/html/body/div[1]/div/div[1]/div/div/div/div/section/div/div/div/div/div[1]/div[2]/div/" \
-             f"div/div/div/div/div[1]/div[3]/div/div[2]/div/div/div/div[2]/div[{index_div}]/div/div[2]/label"
-      element = browser.find_element_by_xpath(path)
+#Get Element
 
-      #Click on Element
+path = "/html/body/div[1]/div/div[1]/div/div/div/div/section/div/div/div/div/div[1]/div[2]/div/div/div/div/div/div[1]/div[3]/div/div[2]/div/div/div/div[2]/div[12]/div/div[2]/div"
 
-      browser.execute_script("arguments[0].scrollIntoView();", element)
-      actions = ActionChains(browser)
-      actions.move_to_element(element)
-      actions.click(element)
-      actions.perform()
+element = browser.find_element_by_xpath(path)
+
+#Click on Element
+actions = webdriver.ActionChains(browser)
+actions.move_to_element(element)
+actions.click(element)
+actions.perform()
+
+time.sleep(3)
 
 #Procurar Resultados
 path = "/html/body/div[1]/div/div[1]/div/div/div/div/article/div/div/a"
 element = browser.find_element_by_xpath(path)
-browser.execute_script("arguments[0].scrollIntoView();", element)
-actions = ActionChains(browser)
+
+actions = webdriver.ActionChains(browser)
 actions.move_to_element(element)
+
+time.sleep(2)
+
 actions.click(element)
 actions.perform()
+
+time.sleep(7)
 
 #Aumentar Número de Páginas
 
 #Clicar caixa
 path = "/html/body/div[1]/div/div[1]/div/div/div/div/div[3]/div[2]/div[2]/span[2]/div/div[1]/span/span/i"
 element = browser.find_element_by_xpath(path)
-browser.execute_script("arguments[0].scrollIntoView();", element)
-actions = ActionChains(browser)
+actions = webdriver.ActionChains(browser)
+
+time.sleep(2)
+
+element.location_once_scrolled_into_view
 actions.move_to_element(element)
 actions.click(element)
 actions.perform()
+
+time.sleep(3)
 
 #Clicar 100 por página
 path = "/html/body/div[3]/div[1]/div[1]/ul/li[6]"
 element = browser.find_element_by_xpath(path)
-actions = ActionChains(browser)
+actions = webdriver.ActionChains(browser)
 actions.move_to_element(element)
 actions.click(element)
 actions.perform()
 
+time.sleep(7)
 
 #Retirar Info primeira página e fazer Documento Pandas
-tabela = pd.read_table(browser)
+
+#Extract
+soup = BeautifulSoup(browser.page_source, 'lxml')
+tables = soup.find_all('table')
+tabela = pd.read_html(str(tables))
+
+#Work
 table_titulos = tabela[0]
 tables_info = tabela[1]
 nomes_colunas = table_titulos.columns
 tables_info.columns = nomes_colunas
 
-for i in range(0, 3):
+time.sleep(3)
+
+for i in range(0, 999999):
       try:
             #Clicar seta próxima página
             path = "/html/body/div[1]/div/div[1]/div/div/div/div/div[3]/div[2]/div[2]/button[1]/i"
             element = browser.find_element_by_xpath(path)
-            browser.execute_script("arguments[0].scrollIntoView();", element)
-            actions = ActionChains(browser)
+            #browser.execute_script("arguments[0].scrollIntoView();", element)
+            element.location_once_scrolled_into_view
+            time.sleep(2)
+            actions = webdriver.ActionChains(browser)
             actions.move_to_element(element)
             actions.click(element)
             actions.perform()
 
-            #Retirar Informação
-            tab = pd.read_table(browser)
+            time.sleep(7)
+
+            # Extract
+            soup = BeautifulSoup(browser.page_source, 'lxml')
+            tables = soup.find_all('table')
+            tabela = pd.read_html(str(tables))
+
+            # Work
             tab_titles = tabela[0]
             tab_info = tabela[1]
             column_names = tab_titles.columns
@@ -127,11 +136,18 @@ for i in range(0, 3):
             #Juntar Info
             tables_info = tables_info.append(tab_info, ignore_index=True)
 
+            time.sleep(3)
+
       except:
             pass
 
+
+time.sleep(3)
+
 #Guardar como csv
 tables_info.to_csv(f"Spain_Company_Names_and_Info.csv", sep = ",", index = False, header = True)
+
+time.sleep(3)
 
 browser.quit()
 
